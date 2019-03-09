f38c78e1b5fcbd0ad4ffabbda42ee1198f4e02e5
Konstantin Gazha <kgazha@gmail.com>
2019-03-09 22:07:17 +0500

    Add data collection from notification_ef and contract_sign files

--- zakupki/analysis.py
+++ zakupki/analysis.py
@@ -7,6 +7,7 @@ django.setup()
 from xml.dom import minidom
 from zakupki.settings import BASE_DIR
 from core import models
+from django.utils.dateparse import parse_datetime, parse_date
 
 
 def get_dirnames(path):
@@ -56,31 +57,106 @@ def __parse_notification_ef_purchase_responsible(node):
     return responsible_org
 
 
+def __parse_notification_ef_child_by_args(node, tag, *args):
+    tag_node = None
+    for _node in node.childNodes:
+        if _node.localName == tag:
+            tag_node = _node
+    if not tag_node:
+        return [None for arg in args]
+    data = []
+    for arg in args:
+        _node = tag_node.getElementsByTagName(arg)
+        if _node:
+            data.append(_node[0].firstChild.data)
+        else:
+            data.append(None)
+    return data
+
+
+def __parse_notification_ef_purchase_objects(nodes):
+    purchase_objects = []
+    for _node in nodes:
+        okpd_code, okpd_name = __parse_notification_ef_child_by_args(_node, 'OKPD', 'code', 'name')
+        okpd2_code, okpd2_name = __parse_notification_ef_child_by_args(_node, 'OKPD2', 'code', 'name')
+        okei_code, okei_national_code, okei_full_name = __parse_notification_ef_child_by_args(_node, 'OKEI',
+                                                                                              'code', 'nationalCode',
+                                                                                              'fullName')
+        price = __parse_node_data_by_tag(_node, 'price')
+        quantity = __parse_notification_ef_child_by_args(_node, 'quantity', 'value')[0]
+        purchase_objects.append({'okpd_code': okpd_code, 'okpd_name': okpd_name,
+                                 'okpd2_code': okpd2_code, 'okpd2_name': okpd2_name,
+                                 'okei_code': okei_code, 'okei_national_code': okei_national_code,
+                                 'okei_full_name': okei_full_name, 'price': price,
+                                 'quantity': quantity})
+    return purchase_objects
+
+
 def __parse_notification_ef_placing_way(node):
-    placing_way_node = node.getElementsByTagName('placingWay')[0]
-    code = placing_way_node.getElementsByTagName('code')[0].firstChild.data
-    name = placing_way_node.getElementsByTagName('name')[0].firstChild.data
-    return code, name
+    element = node.getElementsByTagName('placingWay')
+    if element:
+        return element[0].getElementsByTagName('code')[0].firstChild.data,\
+               element[0].getElementsByTagName('name')[0].firstChild.data
+    else:
+        return None, None
 
 
-def parse_notification_ef(file):
+def __get_minidom_by_file_object(file):
     path_unzipped = os.path.join(BASE_DIR, 'unzipped')
     archive = models.Archive.objects.get(id=file.archive_id)
     resource = models.Resource.objects.get(id=archive.resource_id)
     file_path = os.path.join(path_unzipped, resource.code, archive.name.split('.')[0], file.name)
-    document = minidom.parse(file_path)
+    return minidom.parse(file_path)
+
+
+def parse_notification_ef(file):
+    document = __get_minidom_by_file_object(file)
     purchase_number = __parse_node_data_by_tag(document, 'purchaseNumber')
     purchase_object_info = __parse_node_data_by_tag(document, 'purchaseObjectInfo')
     purchase_responsible = __parse_notification_ef_purchase_responsible(document)
     placing_way_code, placing_way_name = __parse_notification_ef_placing_way(document)
-    print(purchase_number,
-          purchase_object_info,
-          purchase_responsible,
-          placing_way_code,
-          placing_way_name)
-    return (purchase_number,
-            purchase_object_info,
-            purchase_responsible)
+    max_price = __parse_node_data_by_tag(document, 'maxPrice')
+    direct_date = __parse_node_data_by_tag(document, 'directDate')
+    if direct_date:
+        direct_date = parse_datetime(direct_date)
+    doc_publish_date = __parse_node_data_by_tag(document, 'docPublishDate')
+    if doc_publish_date:
+        doc_publish_date = parse_datetime(doc_publish_date)
+    finance_source = __parse_node_data_by_tag(document, 'financeSource')
+    purchase_object_nodes = document.getElementsByTagName('purchaseObject')
+    purchase_objects = __parse_notification_ef_purchase_objects(purchase_object_nodes)
+    return {'purchase_number': purchase_number, 'purchase_object_info': purchase_object_info,
+            'purchase_responsible': purchase_responsible, 'placing_way_code': placing_way_code,
+            'placing_way_name': placing_way_name, 'max_price': max_price,
+            'direct_date': direct_date, 'doc_publish_date': doc_publish_date,
+            'finance_source': finance_source, 'purchase_objects': purchase_objects}
+
+
+def __parse_contract_ef_suppliers(nodes):
+    suppliers = []
+    for _node in nodes:
+        suppliers.append({'inn': __parse_node_data_by_tag(_node, 'inn'),
+                          'kpp': __parse_node_data_by_tag(_node, 'kpp'),
+                          'name': __parse_node_data_by_tag(_node, 'organizationName'),
+                          'fact_address': __parse_node_data_by_tag(_node, 'factualAddress'),
+                          'post_address': __parse_node_data_by_tag(_node, 'postAddress')})
+    return suppliers
+
+
+def parse_contract_ef(file):
+    document = __get_minidom_by_file_object(file)
+    purchase_number = __parse_node_data_by_tag(document, 'purchaseNumber')
+    price = __parse_node_data_by_tag(document, 'price')
+    sign_date = __parse_node_data_by_tag(document, 'signDate')
+    if sign_date:
+        sign_date = parse_date(sign_date.split('+')[0])
+    protocol_date = __parse_node_data_by_tag(document, 'protocolDate')
+    if protocol_date:
+        protocol_date = parse_date(protocol_date.split('+')[0])
+    supplier_nodes = document.getElementsByTagName('supplier')
+    suppliers = __parse_contract_ef_suppliers(supplier_nodes)
+    return {'purchase_number': purchase_number, 'price': price, 'suppliers': suppliers,
+            'sign_date': sign_date, 'protocol_date': protocol_date}
 
 
 def collect_file_description(_dir, _path, _filename):
@@ -95,23 +171,14 @@ def collect_file_description(_dir, _path, _filename):
                 type_control_name = node.localName
     else:
         type_control_name = type_control_node.localName
-    type_control = models.PurchaseTypeControl.objects.get_or_create(code=type_control_name)[0]
+    purchase_type_control = models.PurchaseTypeControl.objects.get_or_create(code=type_control_name)[0]
     file = models.File.objects.get_or_create(name=_filename, archive=archive)[0]
     if not file.archive:
         file.archive = archive
         file.save()
-    if not file.type_control:
-        file.type_control = type_control
+    if not file.purchase_type_control:
+        file.purchase_type_control = purchase_type_control
         file.save()
-    # purchase_number = document.getElementsByTagName('purchaseNumber')
-    # if purchase_number:
-    #     purchase_number = purchase_number[0].firstChild.data
-    # else:
-        # return
-    # purchase = models.Purchase.objects.get_or_create(purchase_number=purchase_number)[0]
-    # file = models.File.objects.get(name=_filename, archive=archive)
-    # file.purchase = purchase
-    # file.save()
 
 
 def collect_file_descriptions():
@@ -123,7 +190,100 @@ def collect_file_descriptions():
                 collect_file_description(_dir, _path, _filename)
 
 
+def __save_purchase_objects_to_database(purchase, objects):
+    for object in objects:
+        if object['okpd_code'] and object['okpd2_code']:
+            okpd = models.OKPD.objects.get_or_create(code=object['okpd_code'])[0]
+            okpd2 = models.OKPD2.objects.get_or_create(code=object['okpd2_code'])[0]
+            purchase_object = models.PurchaseObject.objects.get_or_create(purchase=purchase,
+                                                                          okpd=okpd,
+                                                                          okpd2=okpd2)[0]
+        elif object['okpd_code']:
+            okpd = models.OKPD.objects.get_or_create(code=object['okpd_code'])[0]
+            okpd.name = object['okpd_name']
+            okpd.save()
+            purchase_object = models.PurchaseObject.objects.get_or_create(purchase=purchase,
+                                                                          okpd=okpd)[0]
+        elif object['okpd2_code']:
+            okpd2 = models.OKPD2.objects.get_or_create(code=object['okpd2_code'])[0]
+            okpd2.name = object['okpd2_name']
+            okpd2.save()
+            purchase_object = models.PurchaseObject.objects.get_or_create(purchase=purchase,
+                                                                          okpd2=okpd2)[0]
+        else:
+            continue
+        if object['okei_code'] and object['okei_national_code'] and object['okei_full_name']:
+            okei = models.OKEI.objects.get_or_create(code=object['okei_code'],
+                                                     national_code=object['okei_national_code'],
+                                                     full_name=object['okei_full_name'])[0]
+            purchase_object.okei = okei
+        purchase_object.price = object['price']
+        purchase_object.quantity = object['quantity']
+        purchase_object.save()
+
+
+def __save_suppliers_to_database(purchase, objects):
+    for object in objects:
+        organization = models.Organization.objects.get_or_create(inn=object['inn'])[0]
+        organization.name = object['name']
+        organization.fact_address = object['fact_address']
+        organization.post_address = object['post_address']
+        organization.save()
+        organization_role = models.OrganizationRole.objects.get_or_create(name='supplier')[0]
+        obj, created = models.OrganizationPurchase.objects.get_or_create(organization=organization,
+                                                                         purchase=purchase,
+                                                                         organization_role=organization_role)
+
+
+def save_contract_sign_to_database():
+    contract_sign = models.PurchaseTypeControl.objects.get(code='fcsContractSign')
+    files = models.File.objects.filter(purchase_type_control_id=contract_sign.id)
+    for file in files:
+        data = parse_contract_ef(file)
+        purchase = models.Purchase.objects.get_or_create(purchase_number=data['purchase_number'])[0]
+        file.purchase = purchase
+        file.save()
+        purchase.price = data['price']
+        purchase.sign_date = data['sign_date']
+        purchase.protocol_date = data['protocol_date']
+        purchase.save()
+        __save_suppliers_to_database(purchase, data['suppliers'])
+
+
+def save_notification_ef_to_database():
+    purchase_type_control = models.PurchaseTypeControl.objects.get(code='fcsNotificationEF')
+    files = models.File.objects.filter(purchase_type_control_id=purchase_type_control.id)
+    for file in files:
+        data = parse_notification_ef(file)
+        purchase = models.Purchase.objects.get_or_create(purchase_number=data['purchase_number'])[0]
+        file.purchase = purchase
+        file.save()
+        purchase.object_info = data['purchase_object_info']
+        purchase.max_price = data['max_price']
+        purchase.direct_date = data['direct_date']
+        purchase.doc_publish_date = data['doc_publish_date']
+        finance_source = models.FinanceSource.objects.get_or_create(name=data['finance_source'])[0]
+        purchase.finance_source = finance_source
+        if data['placing_way_code'] and data['placing_way_name']:
+            placing_way = models.PlacingWay.objects.get_or_create(code=data['placing_way_code'],
+                                                                  name=data['placing_way_name'])[0]
+            purchase.placing_way = placing_way
+        organization = models.Organization.objects.get_or_create(inn=data['purchase_responsible']['inn'])[0]
+        organization.reg_num = data['purchase_responsible']['reg_num']
+        organization.kpp = data['purchase_responsible']['kpp']
+        organization.post_address = data['purchase_responsible']['post_address']
+        organization.fact_address = data['purchase_responsible']['fact_address']
+        organization.cons_registry_num = data['purchase_responsible']['cons_registry_num']
+        organization.full_name = data['purchase_responsible']['full_name']
+        organization.save()
+        purchase.save()
+        organization_role = models.OrganizationRole.objects.get_or_create(name='responsible')[0]
+        obj, created = models.OrganizationPurchase.objects.get_or_create(organization=organization,
+                                                                         purchase=purchase,
+                                                                         organization_role=organization_role)
+        __save_purchase_objects_to_database(purchase, data['purchase_objects'])
+
+
 # collect_file_descriptions()
-files = models.File.objects.filter(id__lte=40)
-for file in files:
-    parse_notification_ef(file)
+# save_notification_ef_to_database()
+save_contract_sign_to_database()
--- zakupki/core/models.py
+++ zakupki/core/models.py
@@ -21,8 +21,8 @@ class PurchaseTypeControl(models.Model):
 class PlacingWay(models.Model):
     code = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -30,47 +30,80 @@ class PlacingWay(models.Model):
 
 class Organization(models.Model):
     reg_num = models.CharField(max_length=255)
-    full_name = models.CharField(max_length=255)
+    cons_registry_num = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    full_name = models.TextField(blank=True, null=True)
     post_address = models.CharField(max_length=255, blank=True, null=True)
     fact_address = models.CharField(max_length=255, blank=True, null=True)
-    inn = models.CharField(max_length=255)
-    kpp = models.CharField(max_length=255)
-    email = models.EmailField(max_length=254, blank=True, null=True)
-    site = models.URLField(max_length=200, blank=True, null=True)
-    date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    inn = models.CharField(max_length=255, unique=True)
+    kpp = models.CharField(max_length=255, blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.full_name
 
 
 class FinanceSource(models.Model):
-    name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 
 class Purchase(models.Model):
-    description = models.CharField(max_length=255, blank=True, null=True)
-    purchase_number = models.CharField(max_length=255, blank=True, null=True)
-    placing_way = models.ForeignKey(PlacingWay, on_delete=models.CASCADE, blank=True, null=True)
-    max_price = models.IntegerField(blank=True, null=True)
-    responsible = models.ForeignKey(Organization, on_delete=models.CASCADE, blank=True, null=True)
-    customer = models.ForeignKey(Organization, on_delete=models.CASCADE, blank=True, null=True)
-    finance_source = models.ForeignKey(FinanceSource, on_delete=models.CASCADE, blank=True, null=True)
+    object_info = models.TextField(blank=True, null=True)
+    purchase_number = models.CharField(max_length=255, unique=True)
+    placing_way = models.ForeignKey(PlacingWay, on_delete=models.SET_NULL, blank=True, null=True)
+    max_price = models.FloatField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    sign_date = models.DateField(blank=True, null=True)
+    protocol_date = models.DateField(blank=True, null=True)
+    direct_date = models.DateTimeField(blank=True, null=True)
+    doc_publish_date = models.DateTimeField(blank=True, null=True)
+    finance_source = models.ForeignKey(FinanceSource, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.purchase_number
 
 
+class OrganizationRole(models.Model):
+    name = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class OrganizationPurchase(models.Model):
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, blank=True, null=True)
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE, blank=True, null=True)
+    organization_role = models.ForeignKey(OrganizationRole, on_delete=models.CASCADE, blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class OKPD(models.Model):
     code = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    name = models.TextField(blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class OKPD2(models.Model):
+    code = models.CharField(max_length=255)
+    name = models.TextField(blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -78,28 +111,31 @@ class OKPD(models.Model):
 
 class OKEI(models.Model):
     code = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    national_code = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 
 class PurchaseObject(models.Model):
-    okpd = models.ForeignKey(OKPD, on_delete=models.CASCADE)
-    okei = models.ForeignKey(OKEI, on_delete=models.CASCADE)
+    okpd = models.ForeignKey(OKPD, on_delete=models.SET_NULL, blank=True, null=True)
+    okpd2 = models.ForeignKey(OKPD2, on_delete=models.SET_NULL, blank=True, null=True)
+    okei = models.ForeignKey(OKEI, on_delete=models.SET_NULL, blank=True, null=True)
     purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
-    price = models.IntegerField(blank=True, null=True)
-    date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    price = models.FloatField(blank=True, null=True)
+    quantity = models.FloatField(blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
 
 class Archive(models.Model):
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -108,10 +144,10 @@ class Archive(models.Model):
 class File(models.Model):
     name = models.CharField(max_length=255)
     archive = models.ForeignKey(Archive, on_delete=models.CASCADE)
-    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE, blank=True, null=True)
-    type_control = models.ForeignKey(PurchaseTypeControl, on_delete=models.CASCADE, blank=True, null=True)
-    date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    purchase = models.ForeignKey(Purchase, on_delete=models.SET_NULL, blank=True, null=True)
+    purchase_type_control = models.ForeignKey(PurchaseTypeControl, on_delete=models.SET_NULL, blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
